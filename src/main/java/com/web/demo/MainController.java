313770c92c4339fc6852ba16efff591f16df128d
ji1su2 <yunjeongsug71@gmail.com>
2024-08-20 13:53:15 +0900

    Feat: 주석 반영

--- src/main/java/com/web/demo/MainController.java
+++ src/main/java/com/web/demo/MainController.java
@@ -1,5 +1,11 @@
-package com.web.demo;
+/**
+ * 홈페이지
+ */
 
+
+
+package com.web.demo;
+// 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
